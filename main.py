29a2d8fc97e676d39579a7d6be9a35638cf98b63
Kim Taemin <81472141+barom-sys@users.noreply.github.com>
2021-04-14 17:08:59 +0900

    Update main.py
    잔잔한 오류 해결 및, 새로운 코드

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# -디스코드 봇 바롬
+# 디스코드 봇 바롬
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -9,62 +9,8 @@ client.on('ready', () => {
 });
 
 client.on('message', (message) => {
-    if(message.content ==='도움말') {
-        message.reply('안녕하세요. 저희는 운산고등학교 INPRO 입니다. 바롬의 메인개발자는 K입니다. 요류 발생시 연락주세요.')
-    }
-});
-
-client.on('message', (message) => {
-    if(message.content ==='운산고등학교') {
-        message.reply('자랑스러운 운산고등학교 http://www.woonsan.hs.kr/wah/main/index.htm`')
-    }
-});
-
-client.on('message', (message) => {
-    if(message.content ==='존나') {
-        message.reply('학생들이 자주쓰는 존나는 남성의 성기를 비하하는 말인 좃 에서 파생된 말입니다. ')
-    }
-});
-
-client.on('message', (message) => {
-    if(message.content ==='ㅈㄴ') {
-        message.reply('학생들이 자주쓰는 존나는 남성의 성기를 비하하는 말인 좃 에서 파생된 말입니다.')
-    }
-});
-
-client.on('message', (message) => {
-    if(message.content ==='좃까') {
-        message.reply('남자의 성기를 까라는 말 입니다. 욕을 줄입시다')
-    }
-});
-
-client.on('message', (message) => {
-    if(message.content ==='ㅈㄲ') {
-        message.reply('남자의 성기를 까라는 말 입니다. 욕을 줄입시다')
-    }
-});
-
-client.on('message', (message) => {
-    if(message.content ==='tlqkf') {
-        message.reply('영어로 쳐도 욕은 욕입니다. 씨발의 뜻은 성관계 이므로 욕을 줄입시다.')
-    }
-});
-
-client.on('message', (message) => {
-    if(message.content ==='병신') {
-        message.reply('병신은 장애인을 일컫는 말 입니다. 인터넷 에서 욕을 잘 쓰지 않도록 노력 합시다.')
-    }
-});
-
-client.on('message', (message) => {
-    if(message.content ==='새끼') {
-        message.reply('새끼는 본래 시아우를 가리키던 시아기 에서 유래 되었습니다 하지만 오늘날 새끼는 어린것, 놈 이라는 단어로 쓰이고 있습니다. ')
-    }
-});
-
-client.on('message', (message) => {
-    if(message.content ==='씨발') {
-        message.reply('씨발 이라는 뜻은 성관계라는 뜻 입니다. 욕을 줄입시다. ')
+    if(message.content ==='#원하는 욕') {
+        message.reply('#욕을 인식했을때 대답 할 말')
     }
 });
 
@@ -87,7 +33,7 @@ client.on('message', async message => {
         const user = message.author.id;
         const embed = new Discord.MessageEmbed()
         .setColor('#FF0000')
-        .setDescription('<@${user}> !경고! 욕을 사용하는 모습은 보기 안 좋습니다. 욕을 줄이기 위해 상담을 원하신다면 이 링크를 타고 들어가세요! https://www.cyber1388.kr:447/ ');
+        .setDescription('<@${user}> #위에있는 욕이 인식되었을때 대답 말');
   
         message.channel.send(embed)
 }
